d4d689642a07efbd2e4c9546bf90208624339aa5
Rata13NS <161605200+Rata13NS@users.noreply.github.com>
2024-03-09 13:39:47 +0200

    Add files via upload

---
+++ dictionary.js
@@ -0,0 +1,23 @@
+let savedWords = document.getElementById('savedwords');
+let buttons = [];
+
+function addWord() {
+    let button = document.createElement('button');
+    button.textContent= 'Delete word';
+    button.style.background = "rgba(255, 0, 0, 0.8)";
+    buttons.push(button);
+    savedWords.appendChild(button);
+    let wordIntroduced = document.getElementById('writeWords').value;
+    savedWords.innerHTML += ' ' + wordIntroduced + '<br>';
+    document.getElementById('writeWords').innerHTML = '';
+}
+
+function searchWord() {
+    let wordSearched = document.getElementById('writeWords').value;
+    if (savedWords.textContent.includes(wordSearched + ' ')) {
+        alert('The word is included in dictionary');
+    } else {
+        alert('The word is not included in dictionary');
+    }
+    document.getElementById('writeWords').innerHTML = '';
+}
